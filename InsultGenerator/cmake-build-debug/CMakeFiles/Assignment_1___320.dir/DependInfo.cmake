856af178e33861fb481fab50c441bf2459b9a7eb
Anirudha Verma <yodatime29@gmail.com>
2021-01-01 15:46:54 -0500

    Add files via upload

---
+++ InsultGenerator/cmake-build-debug/CMakeFiles/Assignment_1___320.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/aniru/OneDrive/Desktop/Assignment 1 - 320/TestInsultGenerator.cpp" "C:/Users/aniru/OneDrive/Desktop/Assignment 1 - 320/cmake-build-debug/CMakeFiles/Assignment_1___320.dir/TestInsultGenerator.cpp.obj"
+  "C:/Users/aniru/OneDrive/Desktop/Assignment 1 - 320/insultgenerator_18av8.cpp" "C:/Users/aniru/OneDrive/Desktop/Assignment 1 - 320/cmake-build-debug/CMakeFiles/Assignment_1___320.dir/insultgenerator_18av8.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ InsultGenerator/cmake-build-debug/CMakeFiles/Assignment_1___320.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "Assignment_1___320.exe"
+  "Assignment_1___320.exe.manifest"
+  "Assignment_1___320.pdb"
+  "CMakeFiles/Assignment_1___320.dir/TestInsultGenerator.cpp.obj"
+  "CMakeFiles/Assignment_1___320.dir/insultgenerator_18av8.cpp.obj"
+  "libAssignment_1___320.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assignment_1___320.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
